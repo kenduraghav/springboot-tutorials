b75653295f6bf40452490b590002d256c0c35180
kenduraghav <kenduraghav@gmail.com>
2024-04-26 13:45:53 +0800

    Remove file from remote repository

--- spring-data-jpa/src/main/java/com/thorben/janssen/repositories/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thorben.janssen.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thorben.janssen.entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
