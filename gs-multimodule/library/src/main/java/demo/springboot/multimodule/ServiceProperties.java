c577f1d4cf4adc333d44233f46e23c88deed907d
kenduraghav <kenduraghav@gmail.com>
2024-04-26 13:10:19 +0800

    Added Multi-module springboot example and another demo application for spring boot

---
+++ gs-multimodule/library/src/main/java/demo/springboot/multimodule/ServiceProperties.java
@@ -0,0 +1,20 @@
+package demo.springboot.multimodule;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("service")
+public class ServiceProperties {
+
+  /**
+   * A message for the service.
+   */
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
